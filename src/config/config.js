095a931d01dd0f1fae3a27af45ce95821dea1a1c
Tokioshy <grizzlizyping@gmail.com>
2024-01-03 23:13:24 +0700

    feat (color): add more embed color option

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,8 @@ module.exports = {
   clientId: '1171051892789485592',
   guildId: '1171052119822975096',
   embed: {
-    default: '#ffaf27'
+    default: '#ffaf27',
+    fail: '#c92424',
+    success: '24c74f'
   }
 };
