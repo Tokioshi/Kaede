8cc8ea7f0accd3583587eec16573abfa048996af
Tokioshy <grizzlizyping@gmail.com>
2024-01-03 23:15:17 +0700

    feat (status): add bot status

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -1,9 +1,10 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}!`);
+    client.user.setPresence({ activities: [{ name: 'on progress', type: ActivityType.Watching }], status: 'idle' });
   },
 };
